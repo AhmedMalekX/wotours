ee41747ba778995e6549fb7f0a3efc72cd034f25
Ahmed Malek <ahmedmalekvic@mail.ru>
2020-04-02 11:48:11 +0200

    Add forgot and reset password and send emails with nodemailer :art:

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const { promisify } = require('util');
 
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
@@ -106,3 +107,42 @@ exports.restrictTo = (...roles) => {
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // Get user based on posted email
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    return next(new AppError('There is no user with this email address', 404));
+  }
+  // generate random token
+  const resetToken = user.createPasswordResetToken();
+
+  await user.save({ validateBeforeSave: false });
+  // ssend it to user email
+  const resetURL = `${req.protocol}://${req.get(
+    'host'
+  )}/api/v1/users/resetPassword/${resetToken}`;
+
+  const message = `Forgot your password? Submit a PATCH request with your new  password and passwordConfirm to : ${resetURL} \n  If  you didn't forgot your password please Ignore this Email`;
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Your password reset token (valid for 10 min)',
+      message
+    });
+    res.status(200).json({
+      status: 'success',
+      message: 'Token sent to mail'
+    });
+  } catch (err) {
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false });
+
+    return next(
+      new AppError('There was an error sending the email. Try again later', 500)
+    );
+  }
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {});
--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,5 @@
+const crypto = require('crypto');
+
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
@@ -37,7 +39,9 @@ const userSchema = new mongoose.Schema({
       message: 'Password are not the same!'
     }
   },
-  passwordChangedAt: Date
+  passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetExpires: Date
 });
 
 userSchema.pre('save', async function(next) {
@@ -67,6 +71,19 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamps) {
 
   // false means not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function() {
+  const resetToken = crypto.randomBytes(30).toString('hex');
+
+  this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
 };
 
 const User = mongoose.model('User', userSchema);
