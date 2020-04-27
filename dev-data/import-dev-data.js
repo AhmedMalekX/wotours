f1d2b1ae4eab8b1781ceae0099151465787b32dd
Ahmed Malek <ahmedmalekvic@mail.ru>
2020-04-27 22:44:40 +0200

    Modelling Data and Advanced Mongoose :rocket:

--- dev-data/import-dev-data.js
+++ dev-data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -26,13 +28,21 @@ mongoose
 
 // Read json file
 const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/data/tours-simple.json`, 'utf-8')
+  fs.readFileSync(`${__dirname}/data/tours.json`, 'utf-8')
+);
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/users.json`, 'utf-8')
+);
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/data/reviews.json`, 'utf-8')
 );
 
 // import data  to database
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successful loaded!');
   } catch (err) {
     console.log(err.message);
@@ -44,6 +54,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data Deleted!');
   } catch (err) {
     console.log(err.message);
